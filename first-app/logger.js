9a166a6958752f55d90055c042beae2c761d4c68
Devansh4 <devanshchauhan4@gmail.com>
2018-06-04 17:55:37 +0530

    Lec15

--- first-app/logger.js
+++ first-app/logger.js
@@ -1,7 +1,13 @@
+//(function (exports, require, module, __filename, __dirname) {   //module wrapper function
 var x = "http://myWebSite/log";
 
 function log(message){
     console.log(message);
 }
 
-module.exports.log = log;
+module.exports.log = log; //or module.exports = log as no need to send an object
+
+console.log(__filename);
+console.log(__dirname);
+
+//})
