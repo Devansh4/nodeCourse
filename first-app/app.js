f79bfc43267fbab219c80b39838901eb59f83ab2
Devansh4 <devanshchauhan4@gmail.com>
2018-06-04 19:01:41 +0530

    Lec18

--- first-app/app.js
+++ first-app/app.js
@@ -1,5 +1,12 @@
-const os = require('os');
-let a = os.freemem();
-console.log(a);
-let b = os.totalmem();
-console.log(`Total Memory: ${b}`);  //Template String
+const fs = require('fs');
+
+/*let a = fs.readdirSync('./');
+console.log(a);*/
+
+let b = fs.readdir('./',function(err, result)   //every asynchronous method has a callback function, this one has the parameters error and result, out of which one is null
+{
+    if(err)
+        console.log(`Error: ${b}`);
+    else
+        console.log(result);
+});
