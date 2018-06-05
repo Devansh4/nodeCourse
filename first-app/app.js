f296958caa392e4096d9fd0a07551be9e783558e
Devansh4 <devanshchauhan4@gmail.com>
2018-06-05 11:44:48 +0530

    Lec20

--- first-app/app.js
+++ first-app/app.js
@@ -2,9 +2,9 @@ const EventEmitter = require('events');
 const emitter = new EventEmitter();
 
 //Register a listener
-emitter.on('myEvent', ()=>{
-    console.log("Listener Called");
+emitter.on('myEvent', (args)=>{
+    console.log("Listener Called", args);
 });
 
 //Raise an event
-emitter.emit('myEvent');
+emitter.emit('myEvent',{id:1, url:"https://"}); //event arguments
