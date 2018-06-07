32ed7e418280a7811aab8fc5a89319876591b95e
Devansh4 <devanshchauhan4@gmail.com>
2018-06-07 15:45:56 +0530

    Lec48

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,6 +1,12 @@
 const express = require('express'); //it returns a function
 const app = express();  //it returns an object
 
+const courses = [
+{id:1, name:'course1'},
+{id:2, name:'course2'},
+{id:3, name:'course3'}
+];
+
 app.get('/',(req,res)=>{
     res.send("Hello Devansh");
 });
@@ -9,13 +15,12 @@ app.get('/api/courses',(req,res)=>{
     res.send([1, 2, 3]);
 });
 
-/*app.get('/api/courses/:id',(req,res)=>{   //route parameters - required values
-    res.send(req.params.id);
-});*/
-
-app.get('/api/courses/:year/:month',(req,res)=>{    //localhost:5000/api/courses/2018/5
-    //res.send(req.params);
-    res.send(req.query); // query parameters - optional....//localhost:5000/api/courses/2018/5?sortBy=name
+app.get('/api/courses/:id',(req,res)=>{   //route parameters - required values
+    const course = courses.find(c=> c.id === parseInt(req.params.id));
+    if(!course) //course not found...return 404
+        res.status(404).send("This course with the given id is not available!");
+    else
+        res.send(course);
 });
 
 const port = process.env.port || 3000;
