a02b1e17ce2256e39b8ca64ef0047b47014d4b16
Devansh4 <devanshchauhan4@gmail.com>
2018-06-07 23:01:04 +0530

    lec52

--- express-demo/index.js
+++ express-demo/index.js
@@ -16,7 +16,8 @@ app.get('/',(req,res)=>{
 });
 
 app.get('/api/courses',(req,res)=>{
-    res.send([1, 2, 3]);
+    //res.send([1, 2, 3]);
+    res.send(courses);
 });
 
 app.get('/api/courses/:id',(req,res)=>{   //route parameters - required values
@@ -40,7 +41,7 @@ app.post('/api/courses',(req,res)=>{
     if(result.error)    //input validation with joi
     {
         /*res.status(400).send("Name is required and should be minimum 3 characters");*/    //old validation if-then
-        /*res.status(400).send(result.error);*/ //too complicated info for the client
+        //res.status(400).send(result.error);//too complicated info for the client
         res.status(400).send(result.error.details[0].message);    //simplifies the info the client gets
         return; //to avoid the rest of the function from being executed
     }
@@ -51,6 +52,35 @@ app.post('/api/courses',(req,res)=>{
     courses.push(course);
     res.send(course);   //to make the new course id visible to the client
 });
+
+app.put('/api/courses/:id', (req,res)=>{
+    //Look up for the course
+    //If it doesn't exists, then return 404
+    const course = courses.find(c=> c.id === parseInt(req.params.id));
+    if(!course)
+        res.status(404).send("This course with the given id is not available!");
+    //If exists, the validate
+    //If invalid, return 400- Bad request
+    /*const result = validateCourse(req.body);
+    if(result.error) */ //all time only result.error used....so extract only error
+    const {error} = validateCourse(req.body);   //object destructuring
+    if(error)
+    {
+        res.status(400).send(result.error.details[0].message); 
+        return; 
+    }
+    //If valid, update the course
+    course.name = req.body.name;
+    //return the updated course
+    res.send(course);
+});
+
+function validateCourse(course){
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+    return Joi.validate(course, schema);
+}
 
 const port = process.env.port || 3000;
 app.listen(port, ()=>{
