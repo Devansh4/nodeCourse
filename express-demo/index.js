63583701a27939257aab4a8da178f839cb4415e8
Devansh4 <devanshchauhan4@gmail.com>
2018-06-07 13:31:34 +0530

    Lec48

--- express-demo/index.js
+++ express-demo/index.js
@@ -7,7 +7,16 @@ app.get('/',(req,res)=>{
 
 app.get('/api/courses',(req,res)=>{
     res.send([1, 2, 3]);
-})
+});
+
+/*app.get('/api/courses/:id',(req,res)=>{   //route parameters - required values
+    res.send(req.params.id);
+});*/
+
+app.get('/api/courses/:year/:month',(req,res)=>{    //localhost:5000/api/courses/2018/5
+    //res.send(req.params);
+    res.send(req.query); // query parameters - optional....//localhost:5000/api/courses/2018/5?sortBy=name
+});
 
 const port = process.env.port || 3000;
 app.listen(port, ()=>{
