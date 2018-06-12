ae7afaaecd153ca9c1668d089dc705b0006c8c28
Devansh4 <devanshchauhan4@gmail.com>
2018-06-12 14:22:30 +0530

    Lec71

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,5 +1,15 @@
 console.log('Before');  //blocking
-setTimeout(()=>{        //non-blocking asynchronous
-    console.log('Reading a user from a database...');
-});
+const user = getUser(1);
+console.log(user);
 console.log('After');   //blocking
+
+function getUser(id)
+{
+    setTimeout(()=>{        //non-blocking asynchronous
+        console.log('Reading a user from a database...');
+        return {                    //what we are returning will be available to the function call after 2 seconds, so is undefined
+            id: id,
+            gitHubUsername: 'dc'
+        }
+    },2000);
+}
