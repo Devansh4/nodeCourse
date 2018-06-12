28e02fe07b2bb325198672def852d17ea9bffd67
Devansh4 <devanshchauhan4@gmail.com>
2018-06-12 17:52:24 +0530

    Lec74

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,20 +1,20 @@
 console.log('Before');  //blocking
-getUser(1, function(user){
-    getRepositories(user.gitHubUsername, (repos)=>{
-        getCommits(repos, (commits)=>{
-            console.log(commits);   //callback hell
-        });
-    });
-});
+getUser(1, getRepositories);
+
 console.log('After');   //blocking
 
-//Synchronous
-console.log('Before');
-const user = getUser(1);
-const repos = getRepositories(user.gitHubUsername);
-const commits = getCommits(repos[0]);
-console.log('After');
+function displayCommits(commits){
+    console.log(commits);
+}
 
+function getCommits(repos){
+    getCommits(repos, displayCommits);
+}
+
+function getRepositories(user)
+{
+    getRepositories(user.gitHubUsername, getCommits);
+}
 function getUser(id, callback)
 {
     setTimeout(()=>{        //non-blocking asynchronous
@@ -27,7 +27,7 @@ function getRepositories(username, callback){
     setTimeout( ()=>{
     console.log('Calling the GitUHb API...');
     callback(['repo1','repo2','repo3']);}, 2000);
-}
+};
 
 function getCommits(repo, callback){
     callback('Commit1');
