83f92211728bcddcbc52fe29c9e87811e03e2668
Devansh4 <devanshchauhan4@gmail.com>
2018-06-13 13:35:34 +0530

    Lec79

--- async-demo/promise-api.js
+++ async-demo/promise-api.js
@@ -1,7 +1,22 @@
-/*//creating a promise that has already been resolved, useful for unit testing
-const p = Promise.resolve({id: 1});
-p.then(result => console.log(result));*/
+const p1 = new Promise((resolve) => {    //only resolve is needed
+    setTimeout(() => {
+        console.log("async operation 1...");
+        resolve(1);
+        //reject(new Error('because something failed'));
+    },2000);
+});
+const p2 = new Promise((resolve) => {
+    setTimeout(() => {
+        console.log("async operation 2...");
+        resolve(2);
+    },2000);
+});
+//running promises in parallel
+Promise.all([p1, p2])   //supply an array of promises   //there is no real concurrencies(multi-thread)
+.then(result => console.log(result))   //result array will have two values
+.catch(err => console.log(err.message));    //if error in any one, this will show the error
 
-//creating a rejected promise
-const p = Promise.reject(new Error('Reason for error...'));
-p.catch(err => console.log(err));
+//return as soon as one of the promise is completed
+Promise.race([p1, p2])  
+.then(result => console.log(result))  //result is the value of the first fulfilled promise
+.catch(err => console.log(err.message));   
