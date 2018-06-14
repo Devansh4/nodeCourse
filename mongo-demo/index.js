d712fb036a10712d5746fb12471d7ec3661d2b93
Devansh4 <devanshchauhan4@gmail.com>
2018-06-14 12:18:38 +0530

    Lec91

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -31,9 +31,11 @@ console.log(result);
 
 async function getCourses(){
     const courses = await Course
-        .find({ author: 'Mosh', isPublished: true })
+        //.find({ author: 'Mosh', isPublished: true })
         //.find({ price: { $gte: 10, $lte: 20 } })  //pass an object with one of the comparison operator(eq,ne,lt,gt,lte,gte,in,nin) as the key.$ indicates that this is an operator
         //.find({price: { $in: [10,15,20]}})
+        .find() //logical operator(and, or). and is similar to the filter directly in find()
+        .or([ {author: 'Mosh'}, {isPublished: true} ]) //pass two object, with each object as a filter
         .limit(10)
         .sort({ name: 1})
         .select({ name: 1, tags: 1});
