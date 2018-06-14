856e859edf2e32e2a4645d3017a7c737d36a9722
Devansh4 <devanshchauhan4@gmail.com>
2018-06-14 16:48:25 +0530

    Lec96

--- mongo-demo/solution1.js
+++ mongo-demo/solution1.js
@@ -18,10 +18,10 @@ const courseSchema = mongoose.Schema(
 const Course = mongoose.model('Course',courseSchema);
 
 async function getCourses(){
-    const courses = await Course.find({ tags: 'backend' , isPublished: true })
-                                    .sort({name: 1})                        //sort(-name)
-                                    .select({name:1, author:1});
-    return courses;
+    return await Course.find({ isPublished: true })
+                        .or([{tags:'frontend'}, {tags: 'backend'}])
+                        .sort('-price')                        //sort(-name)
+                        .select('name author price');
 }
 async function run(){
 const courses = await getCourses();
