130673308083141c095221eb9deda683b4d1a4b4
Devansh4 <devanshchauhan4@gmail.com>
2018-06-14 11:53:17 +0530

    Lec89

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -14,8 +14,9 @@ const courseSchema = mongoose.Schema(
     }
 );
 
-async function createCourse(){
 const Course = mongoose.model('Course',courseSchema);   //creates a class from the schema, course in the parameters is the singular name of the collection
+
+async function createCourse(){
 const course = new Course({
     name: "Angular course",
     author: 'Mosh',
@@ -26,4 +27,14 @@ const course = new Course({
 const result = await course.save();   //async operation as it will take some time. Returns a promise
 console.log(result);
 }
-createCourse();
+//createCourse();
+
+async function getCourses(){
+    const courses = await Course
+        .find({ author: 'Mosh', isPublished: true })
+        .limit(10)
+        .sort({ name: 1})
+        .select({ name: 1, tags: 1});
+    console.log(courses);
+}
+getCourses();
